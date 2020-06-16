848620a4ea30a51ba10e78172aa0d704f9bc2565
Mallika-93 <mallika.chaulagain@gmail.com>
2020-06-15 21:44:46 -0500

    few changes made

--- client/js/script.js
+++ client/js/script.js
@@ -1,26 +1,44 @@
-function getAlerted(){
-	// var name = "Ram";
-	// var number = 20;
-	// document.getElementById('demo').innerHTML = name;
+// function getAlerted(){
+// 	// var name = "Ram";
+// 	// var number = 20;
+// 	// document.getElementById('demo').innerHTML = name;
 	
-	var person1= {name:"Rama", age:25, location:'San Francisco', graduated:true};
-	var person2 = {name:"Medina", age:23, location:'Las Vegas', graduated:false};
-	var person3 = {name:"Alex", age:25, location:'Los Angeles', graduated:true};
+// 	var person1= {name:"Rama", age:25, location:'San Francisco', graduated:true};
+// 	var person2 = {name:"Medina", age:23, location:'Las Vegas', graduated:false};
+// 	var person3 = {name:"Alex", age:25, location:'Los Angeles', graduated:true};
 
-	// document.getElementById('demo').innerHTML = 
-	// person2.name + " is " + person2.age + " years old";
+// 	// document.getElementById('demo').innerHTML = 
+// 	// person2.name + " is " + person2.age + " years old";
     
-    var people = [person1,person2,person3];
-    //console.log(people);
-    for(var i=0; i < people.length; i++){
-    	//document.getElementById('demo').innerHTML += people[i].name;  
-    	if(people[i].graduated == true){
-    		document.getElementById('demo').innerHTML += people[i].name + ' ' +
-    		"is Graduated." + "<br>";
-    	}
-    	else{
-    		document.getElementById('demo').innerHTML += people[i].name + ' ' +
-    		"is not Graduated." + "<br>";
-    	}
-    }
-}
+//     var people = [person1,person2,person3];
+//     //console.log(people);
+//     for(var i=0; i < people.length; i++){
+//     	//document.getElementById('demo').innerHTML += people[i].name;  
+//     	if(people[i].graduated == true){
+//     		document.getElementById('demo').innerHTML += people[i].name + ' ' +
+//     		"is Graduated." + "<br>";
+//     	}
+//     	else{
+//     		document.getElementById('demo').innerHTML += people[i].name + ' ' +
+//     		"is not Graduated." + "<br>";
+//     	}
+//     }
+// }
+
+var zodiacs = [ 
+{
+	name: "Aries",
+	month: "March 21-April-20",
+	description: "Aries is rules by Mars"
+},
+{
+	name: "Taurus"
+	month: "April 20 to may 20",
+	description: "Taurus is ruled by venus"
+},
+
+	name: "Gemini"
+	month: "May 21 to June 20",
+	description: "Gemini mind is a busy machine"
+
+]
